bbe893fa7ea309bbb91584ad889d303962100155
Vitor Valandro <vitormateusd@gmail.com>
2022-07-15 23:09:40 -0300

    fix: convert list display to 2d matrix in PygameRenderer

--- src/implementations/pgRenderer.py
+++ src/implementations/pgRenderer.py
@@ -1,22 +1,14 @@
 import pygame
 from ..chip8.Renderer import Renderer
 
-def matriz(col, linhas):
-    matriz = []
-    for i in range(linhas):
-        l = []
-        for j in range(col):
-            l.append(0)
-        matriz.append(l)
 
-    return matriz
 class PygameRenderer(Renderer):
     def __init__(self, scale):
         super().__init__()
         self.scale = scale
         self.screen = pygame.display.set_mode(
             (self.cols * scale, self.rows * scale))
-        self.display = matriz(self.cols, self.rows)
+        self.display = self.matrix_generator(self.cols, self.rows)
 
         pygame.display.set_caption('CHIP-8 interpreter renderer')
 
@@ -34,9 +26,8 @@ class PygameRenderer(Renderer):
         self.display[y][x] ^= 1
         return self.display[y][x] == 0
 
-
     def clear(self):
-        self.display = [[0] * self.cols] * self.rows
+        self.display = self.matrix_generator(self.cols, self.rows)
 
     def render(self):
         black = (0, 0, 0)
@@ -48,8 +39,19 @@ class PygameRenderer(Renderer):
             y = i * self.scale
             for j in range(self.cols):
                 x = j * self.scale
-                
+
                 if self.display[i][j] == 1:
-                    pygame.draw.rect(self.screen, white, (x, y, self.scale, self.scale))
+                    pygame.draw.rect(self.screen, white,
+                                     (x, y, self.scale, self.scale))
 
         pygame.display.update()
+
+    def matrix_generator(self, col, lines):
+        matrix = []
+        for i in range(lines):
+            l = []
+            for j in range(col):
+                l.append(0)
+            matrix.append(l)
+
+        return matrix
